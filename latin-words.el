88c2bc9b4a8ac70209ce395aded10da604c78513
EnigmaCurry <ryan@enigmacurry.com>
2025-01-10 12:08:36 -0700

    latin-words.el

---
+++ latin-words.el
@@ -0,0 +1,20 @@
+;; latin-words.el --- A Latin-English dictionary -*- lexical-binding: t; -*-
+;;
+;; Author: EnigmaCurry
+;; URL: https://github.com/enigmacurry/latin-words
+;; Version: 0.1
+;; Package-Requires: ((emacs "27.1"))
+;; Keywords: dictionary, latin
+;; SPDX-License-Identifier: MIT AND CC-BY-SA-3.0
+;;
+;; This file is not part of GNU Emacs.
+
+;;; Commentary:
+
+;; This is a Latin to English Dictionary based upon the work "A Latin
+;; Dictionary" by Charlton T. Lewis and Charles Short, published by
+;; the Perseus Digital Library at Tufts University CC-BY-SA 3.0
+;;
+;; See LICENSE.txt
+
+(provide 'latin-words)
